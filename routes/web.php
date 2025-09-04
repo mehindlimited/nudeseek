b337c8b6f9fc551fe3756dc650a81f37e9aa2fd0
Simone Fornara <info@mehind.com>
2025-09-04 05:23:24 +0200

    Fixing pagination

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,7 @@ use App\Http\Controllers\VideoController;
 use App\Http\Controllers\IndexController;
 
 Route::get('/', [IndexController::class, 'index'])->name('index');
-Route::get('/{page}/', [IndexController::class, 'index'])
-    ->whereNumber('page');
+
 Route::get('/video/{id}/{slug}', [VideoController::class, 'show'])
     ->whereNumber('id')
     ->name('video.show');
@@ -16,3 +15,11 @@ Route::get('/video/{id}/{slug}', [VideoController::class, 'show'])
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
 
 Route::get('/test', [TestController::class, 'index'])->name('test.index');
+
+Route::get('/{page}/', [IndexController::class, 'index'])
+    ->whereNumber('page');
+Route::get(
+    '/{page}',
+    fn(int $page) =>
+    redirect()->route('index', ['page' => $page], 301)
+)->whereNumber('page');
