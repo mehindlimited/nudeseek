6535e42d9357fb0c6278be46f5d27d95a4df5842
Simone Fornara <info@mehind.com>
2025-09-04 09:17:01 +0200

    Fixing Category Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'slug', 'description', 'image', 'target_id'];
+    protected $fillable = ['name', 'slug', 'description', 'image', 'target_id', 'legacy', 'is_extreme'];
 
     public function target()
     {
